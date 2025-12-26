d0728749c31fd0c7912cd781d8f178c8ea0dbd4e
Yhon Vivas <cactu@Mac-mini-de-Yhon.local>
2025-12-26 12:44:32 -0300

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,18 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Yhon Vivas on 24-12-25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
